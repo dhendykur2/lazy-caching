d3cb08a93f021b26bf85ce5762efa1577603e16a
dk <OVO@Dhendy-Ganteng.local>
2020-03-20 11:27:39 +0700

    add lazy caching on in application memory level

---
+++ README.md
@@ -0,0 +1,44 @@
+# In Memory Cache (Application Level)
+In Memory Cache is used for caching the data to memory application. The purpose of this package is to save a global data in memory, so the other file can access the data easily.
+
+# Usage
+```js
+const cache = require('./cache');
+
+// parameters: key -> String, value -> any
+// store the value with key
+cache.set('key', { value: 'value' });
+
+
+// parameters: key -> String, value -> any, expiration -> Number in seconds
+// store the value with key
+cache.set('key1', { value: 'value1' }, 120);
+
+// parameters: key -> String
+// get value by key
+const data = cache.get('key');
+console.log(data);
+// { value: 'value' }
+
+// remove by key
+const deletedData = cache.del('key');
+console.log(deletedData);
+// { value: 'value' }
+
+cache.set('key', { value: 'value' });
+cache.set('key1', { value: 'value' });
+cache.set('key2', { value: 'value' });
+
+// clear all data that stored in cache
+cache.clear();
+
+const [data1, data2, data3] = [cache.get('key'), cache.get('key1'), cache.get('key2')];
+console.log(data1); // null
+console.log(data2); // null
+console.log(data3); // null
+
+```
+
+# Why not redis?
+instead we accessing the persistent constant value from redis, its better to store that data in our application memory.
+
---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+const checkLength = (str = '') => str.length > 255;
+
+/**
+   * Cache Constructor
+   * This is Application Level Memory Caching
+*/
+function Cache() {
+  const _cache = {};
+
+  /**
+   * Set Function
+   * @param {String} key
+   * @param {any} value
+   * @param {Number} ex Number in seconds
+   * @return {any}
+   */
+  this.set = function(key, value, ex) {
+    if (!key || !value) throw new Error('Key/Value cannot be empty');
+    if (typeof key != 'string') throw new Error('Key Must be String.');
+    if (ex && typeof ex != 'number') throw new Error('Expiration must be a Number/Integer')
+
+    if (ex) {
+      let a = new Date()
+      ex = a.setSeconds(a.getSeconds() + ex);
+    }
+    _cache[key] = JSON.stringify([value, ex]);
+    return value;
+  };
+
+  /**
+   * Get Function
+   * @param {String} key
+   * @return {any}
+   */
+  this.get = function(key) {
+    if (!key || typeof key != 'string') throw new Error('Key cannot be empty and must be string');;
+
+    const val = _cache[key];
+    if (!val) return undefined;
+    const [value, ex] = JSON.parse(val);
+    if (ex && Date.now() - ex > 0) {
+      _cache[key] = null;
+      delete _cache[key];
+      return undefined;
+    }
+
+    return value
+  };
+
+  /**
+   * Del Function
+   * @param {String} key
+   * @return {any}
+   */
+  this.del = function(key) {
+    if (!key || typeof key != 'string') throw new Error('Key cannot be empty and must be string');;
+
+    const data = _cache[key];
+    if (!data) return null;
+    // Set null to clear the memory also
+    _cache[key] = null;
+    delete _cache[key];
+
+    return JSON.parse(data);
+  };
+
+  /**
+   * Clear all the cache
+   */
+  this.clear = function() {
+    for (const key in _cache) {
+      if (_cache.hasOwnProperty(key)) {
+        // Set null to clear the memory also
+        _cache[key] = null;
+        delete _cache[key];
+      }
+    }
+  };
+}
+
+module.exports = new Cache();
+
